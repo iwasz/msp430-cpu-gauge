840e3c797ef95e48072c403a3e5d507257aad867
Iwasz <lukasz.iwaszkiewicz@gmail.com>
2014-11-02 12:44:40 +0100

    Initial commit. Previously this project was in a google.code SVN repo, but not much commits were there.

---
+++ build/configure.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cmake -DCMAKE_BUILD_TYPE='Debug' -DDRIVERLIB="/home/iwasz/Documents/workspace-CDT/MSP430ware_1_90_00_30/driverlib/driverlib/MSP430F5xx_6xx" -DUSBLIB="/home/iwasz/Documents/workspace-CDT/MSP430USBDevelopersPackage_4_10_02/MSP430_USB_Software/MSP430_USB_API" .
