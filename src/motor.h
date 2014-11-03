1358a2524523f0320add5f732d86187c2fb304bf
Iwasz <lukasz.iwaszkiewicz@gmail.com>
2014-11-03 17:06:44 +0100

    Added basic functionality for motor controll, modified configuration for 5506µC.

---
+++ src/motor.h
@@ -0,0 +1,14 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@gmail.com                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+extern void initPWM (void);
+
+#endif /* MOTOR_H_ */
